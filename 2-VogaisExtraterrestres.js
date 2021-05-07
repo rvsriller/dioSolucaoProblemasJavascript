f91c3a3e77a3df0ab4b29df536fbc66a7b8748c6
Riller <rvsriller@gmail.com>
2021-05-07 00:39:40 -0300

    Desafio 2: Vogais Extraterrestres

---
+++ 2-VogaisExtraterrestres.js
@@ -0,0 +1,19 @@
+/*
+
+  Dado uma sequência de vogais, em um determinado alfabeto alienígena, 
+  esse algoritmo contabiliza, em um texto escrito com o mesmo alfabeto, 
+  quantas vogais o mesmo possui.
+
+  Haverá diversos casos de teste. Cada caso de teste é formado por duas linhas. 
+  A primeira linha informa uma palavra, formada por todas as vogais alienígenas 
+  de um determinado planeta. A segunda linha contém uma frase formada por 
+  letras do mesmo alfabeto. A entrada termina com fim de arquivo.
+
+
+  Exemplo
+  vogaisDosAliens: aeiou
+  frase: o rato roeu a roupa do rei de roma
+
+  retorno (16)
+*/
+
