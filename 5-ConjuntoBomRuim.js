34c1db7c16d1947fc1b29e98a90c24bb8a43a33d
Riller <rvsriller@gmail.com>
2021-05-11 01:11:32 -0300

    Desafio 5: Conjunto Bom e Ruim

---
+++ 5-ConjuntoBomRuim.js
@@ -0,0 +1,11 @@
+/*
+
+    Nesse algoritmo você deverá descobrir se um conjunto de palavras é bom ou ruim. 
+    Por definição, um conjunto é bom quando nenhuma palavra desse conjunto é um prefixo de outra palavra. 
+    Caso contrário, é considerado um conjunto ruim.
+
+    Por exemplo, {dbc, dae, dbcde} é um conjunto ruim, pois dbc é um prefixo de dbcde. 
+    Quando duas palavras são idênticas, definimos como uma sendo prefixo da outra.
+
+*/
+
