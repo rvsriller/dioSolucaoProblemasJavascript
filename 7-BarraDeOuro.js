77b8b9ad74abab7a06bbc2f66a886c394fe30e39
Riller <rvsriller@gmail.com>
2021-05-16 00:11:00 -0300

    Desafio 7: Barrra de Ouro

--- 7-BarraDeOuro.js
+++ 7-BarraDeOuro.js
@@ -1,7 +1,8 @@
 /*
 
     José como é o Rei, contratou o seu time para, dados a quantidade de ouro a ser recolhido em cada feudo (em kg),
-    a lista das estradas do reino, com os respectivos comprimentos (em km) e a capacidade de carga da carruagem real (em kg), determine qual é a mínima distância que a carruagem deve percorrer para recolher todo o ouro devido.
+    a lista das estradas do reino, com os respectivos comprimentos (em km) e a capacidade de carga da carruagem real (em kg), 
+    determine qual é a mínima distância que a carruagem deve percorrer para recolher todo o ouro devido.
 
     Entrada
     A primeira linha contém dois inteiros N e C indicando respectivamente o número de cidades e a capacidade de carga da carruagem (2 ≤ N ≤ 104 e 1 ≤ C ≤ 100). O feudo principal é identificado pelo número 1 e os outros feudos são identificadas por inteiros de 2 a N . A segunda linha contém N inteiros Ei representando a quantidade de imposto devido por cada feudo i (0 ≤ Ei ≤ 100 para 1 ≤ i ≤ N ). Cada uma das N-1 linhas seguintes contém três inteiros A , B e C , indicando que uma estrada liga o feudo A e o feudo B (1 ≤ A, B ≤ N ) e tem comprimento C (1 ≤ C ≤ 100).
